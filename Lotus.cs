056b78c5b78f6e6f325e6104b56ecc67a92b3ef4
Pipsquexk <LHGames21@gmail.com>
2022-07-29 12:08:17 -0500

    Further cleanup

--- Lotus.cs
+++ Lotus.cs
@@ -36,15 +36,14 @@ namespace LotusLoader
                 {
                     MelonLogger.Log("mainType is not null");
                 }
+                
                 lotusMainTypeConstructor = rawMainType.GetConstructor(Type.EmptyTypes);
                 mainType = lotusMainTypeConstructor.Invoke(new object[0]);
-                foreach (MethodInfo methodFucker in rawMainType.GetMethods())
-                {
-                    MelonLogger.Log(methodFucker.Name);
-                }
+                
                 OnApplicationStart = rawMainType.GetMethod("OnApplicationStart");
                 OnLevelWasLoaded = rawMainType.GetMethod("OnLevelWasLoaded");
                 OnUpdate = rawMainType.GetMethod("OnUpdate");
+                
                 if (OnApplicationStart == null)
                 {
                     MelonLogger.Log("OnApplicationStart is null");
